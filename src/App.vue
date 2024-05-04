76e73ef7e28eb48eb7d8e17ed490c808150f4b14
Denis <55382692+DenisDolmatov2020@users.noreply.github.com>
2024-05-04 17:39:48 +0700

    Init

---
+++ src/App.vue
@@ -0,0 +1,166 @@
+<template>
+  <div class="container">
+    <div class="flip-boxes">
+      <div
+          v-for="n in 3"
+          :key="n"
+          :class="['flip-box', { 'flip-box--flipped': n === 2 && isFlipped }]"
+      >
+        <div class="flip-box__front">
+        </div>
+        <div
+            v-if="n === 2"
+            class="flip-box__back"
+        >
+          <div class="flip-box__back-top">
+            <div class="flip-box__back-top__value">
+              <div>7</div>
+              <div>&#9829;</div>
+            </div>
+
+          </div>
+          <div class="flip-box__back-middle">
+            &#9829;
+          </div>
+
+          <div class="flip-box__back-bottom">
+            <div class="flip-box__back-bottom__value">
+              <div>7</div>
+              <div>&#9829;</div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <UIButton
+      :is-flip="isFlipped"
+      @click="toggleFlip"
+    />
+  </div>
+
+</template>
+<script setup lang="ts">
+import { ref } from 'vue';
+import UIButton from './UIButton.vue';
+
+const isFlipped = ref(false);
+
+const toggleFlip = () => {
+  isFlipped.value = !isFlipped.value;
+};
+
+</script>
+<style scoped>
+.container {
+  margin-top: 250px;
+  max-width: 100vw;
+  flex-direction: column;
+  justify-content: end;
+  align-items: center;
+  gap: 5rem;
+}
+
+.flip-boxes {
+  display: flex;
+  justify-content: center;
+  gap: 2vw;
+  margin: 0 auto;
+}
+
+.flip-box {
+  top: 0;
+  display: flex;
+  align-content: stretch;
+  width: 30vw;
+  max-width: 200px;
+  max-height: 240px;
+  height: 36vw;
+  flex-wrap: wrap;
+  position: relative;
+  padding: 0;
+  border-top: 0;
+  -webkit-perspective: 1000px;
+  perspective: 1000px;
+  -webkit-transition: transform 1s;
+  transition: transform 1s;
+}
+
+.flip-box--flipped {
+  transform: translateY(-100%);
+}
+
+.flip-box__front,
+.flip-box__back {
+  position: relative;
+  background-image: linear-gradient(45deg, #ffffff 3px, red 3px, red 5px, #ffffff 5px),
+                    linear-gradient(-45deg, #ffffff 3px, red 3px, red 5px, #ffffff 5px);
+  background-size: 4px 4px;
+  border-radius: 8px;
+  flex: 0 0 100%;
+  -webkit-transition: all 1s .1s cubic-bezier(.5,1,.5,1);
+  transition: all 1s .1s cubic-bezier(.5,1.3,.5,1.3);
+  transform-style: preserve-3d;
+  background-position: center;
+  color: red;
+}
+
+.flip-box__back {
+  background: #ffffff;
+  position: absolute;
+  left: 0;
+  top: 0;
+  bottom: 0;
+  width: 100%;
+  z-index: -1;
+  transform: rotateY(-180deg);
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  flex-grow: initial;
+}
+
+.flip-box__back-top,
+.flip-box__back-bottom {
+  height: 1em;
+  font-size: 2em;
+}
+
+.flip-box__back-bottom {
+  width: 100%;
+  display: flex;
+  justify-content: end;
+}
+
+.flip-box__back-top__value,
+.flip-box__back-bottom__value {
+  padding: 1vh;
+}
+
+.flip-box__back-bottom__value {
+  transform: rotate(180deg);
+}
+
+.flip-box__back-middle {
+  width: 100%;
+  height: 0;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  font-size: 4em;
+}
+
+.flip-box__front {
+  z-index: 10;
+}
+
+.flip-box--flipped .flip-box__back {
+  transform: rotateY(0deg);
+  z-index: 10;
+}
+
+.flip-box--flipped .flip-box__front {
+  transform: rotateY(180deg);
+  z-index: -1;
+  box-shadow: 0 0 10px black;
+}
+</style>
---
+++ src/UIButton.vue
@@ -0,0 +1,121 @@
+<script setup lang="ts">
+interface Props {
+  isFlip: boolean;
+}
+
+const props = defineProps<Props>();
+</script>
+
+<template>
+  <button class="button">
+    <span></span>
+    <span></span>
+    {{ isFlip ? 'Close' : 'Open' }}
+  </button>
+</template>
+
+<style scoped>
+@import url('https://fonts.googleapis.com/css2?family=Poppins:wght@100;300;400;500;600;700;800;900&display=swap');
+
+.button {
+  cursor: pointer;
+  bottom: 20px;
+  right: 20px;
+  position: absolute;
+  padding: 16px 30px;
+  font-size: 1.5rem;
+  color: #1e9bff;
+  border: none;
+  text-shadow: 0 0 15px #1e9bff;
+  text-decoration: none;
+  text-transform: uppercase;
+  letter-spacing: 0.1rem;
+  transition: 0.5s;
+  z-index: 1;
+  border-radius: 20px;
+}
+
+.button:hover {
+  color: #fff;
+  border: 2px solid rgba(0, 0, 0, 0);
+  box-shadow: 0 0 0px #1e9bff;
+}
+
+.button::before {
+  content: '';
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  background: #1e9bff;
+  z-index: -1;
+  transform: scale(0);
+  transition: 0.5s;
+}
+
+.button:hover::before {
+  transform: scale(1);
+  transition-delay: 0.5s;
+  box-shadow: 0 0 10px #1e9bff,
+  0 0 30px #1e9bff,
+  0 0 60px #1e9bff;
+}
+
+.button span {
+  position: absolute;
+  background: #1e9bff;
+  pointer-events: none;
+  border-radius: 2px;
+  box-shadow: 0 0 10px #1e9bff,
+  0 0 10px #1e9bff,
+  0 0 20px #1e9bff,
+  0 0 40px #1e9bff,
+  0 0 600px #1e9bff;
+  transition: 0.5s ease-in-out;
+  transition-delay: 0.25s;
+}
+
+.button:hover span {
+  opacity: 0;
+  transition-delay: 0s;
+}
+
+.button span:nth-child(1) {
+  width: 20px;
+  height: 4px;
+}
+
+.button:hover span:nth-child(1) {
+  transform: translateX(0);
+}
+
+.button span:nth-child(2) {
+  width: 4px;
+  height: 20px;
+}
+
+.button:hover span:nth-child(1) {
+  transform: translateY(0);
+}
+
+.button span:nth-child(1) {
+  top: calc(30% - 2px);
+  left: -30px;
+  transform-origin: left;
+}
+
+.button:hover span:nth-child(1) {
+  left: 30%;
+}
+
+.button span:nth-child(2) {
+  left: calc(30% - 2px);
+  top: -30px;
+  transform-origin: top;
+}
+
+.button:hover span:nth-child(2) {
+  top: 30%;
+}
+</style>
